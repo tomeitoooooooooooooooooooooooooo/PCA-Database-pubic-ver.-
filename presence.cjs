f769be90f7eff3e20ae28250c64c332a25bc4916
tomeitoooooooooooooooooooooooooo <Atoaspoon@outlook.jp>
2025-08-10 03:48:03 +0900

    Update presence.cjs

--- presence.cjs
+++ presence.cjs
@@ -1,6 +1,6 @@
 client.once('ready', () => {
   client.user.setPresence({
-    activities: [{ name: '指名手配', type: 0 }], 
+    activities: [{ name: '指名手配を登録中', type: 3 }], // Watching 指名手配を登録中
     status: 'online',
   });
   console.log(`Logged in as ${client.user.tag}!`);
